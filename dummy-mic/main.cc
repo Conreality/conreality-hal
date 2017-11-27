8716a3a2c520c721916ef2519dc653d43b1692a2
Arto Bendiken <arto@conreality.org>
2017-11-27 16:44:05 +0100

    Defined dummy drivers for important devices.

---
+++ dummy-mic/main.cc
@@ -0,0 +1,14 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <assert.h> /* for assert() */
+#include <cstdio>   /* for std::printf() */
+#include <cstdlib>  /* for EXIT_SUCCESS */
+
+int
+main(void) {
+  return EXIT_SUCCESS; // TODO
+}
