773d5183465b26c4fb5c94b1facc9df6c401335e
Arto Bendiken <arto@conreality.org>
2017-11-28 15:50:51 +0100

    Improved error handling in the dummy GPS driver.

--- dummy-gps/main.cc
+++ dummy-gps/main.cc
@@ -9,7 +9,7 @@
 #include <assert.h> /* for assert() */
 #include <chrono>   /* for std::chrono */
 #include <cstdio>   /* for stderr, std::fprintf() */
-#include <cstdlib>  /* for EXIT_SUCCESS, std::atof() */
+#include <cstdlib>  /* for EXIT_*, std::atof() */
 #include <random>   /* for std::default_random_engine */
 #include <thread>   /* for std::this_thread */
 #include <unistd.h> /* for getopt() */
@@ -54,25 +54,31 @@ main(int argc, char* const argv[]) {
   if (argc >= 2) longitude = atof(argv[1]);
   if (argc >= 3) return usage(program), EXIT_FAILURE;
 
-  /* Open input/output streams: */
-  conreality::ddk::input in;
-  conreality::ddk::output out;
+  try {
+    /* Initialize the random-number generator: */
+    std::default_random_engine rand;
+    std::uniform_real_distribution<double> noise_dist{-1.0, 1.0};
 
-  /* Initialize the random-number generator: */
-  std::default_random_engine rand;
-  std::uniform_real_distribution<double> noise_dist{-1.0, 1.0};
+    /* Open input/output streams: */
+    conreality::ddk::input in;
+    conreality::ddk::output out;
 
-  /* Generate sensor readings to the output stream: */
-  while (in.is_open()) {
-    double lat = latitude, lon = longitude;
-    if (noise > 0.0) {
-      lat += noise * noise_dist(rand);
-      lon += noise * noise_dist(rand);
+    /* Generate sensor readings to the output stream: */
+    while (in.is_open()) {
+      double lat = latitude, lon = longitude;
+      if (noise > 0.0) {
+        lat += noise * noise_dist(rand);
+        lon += noise * noise_dist(rand);
+      }
+      out.write_tuple(lon, lat);
+
+      std::this_thread::sleep_for(interval);
     }
-    out.write_tuple(lon, lat);
 
-    std::this_thread::sleep_for(interval);
+    return EXIT_SUCCESS;
   }
-
-  return EXIT_SUCCESS;
+  catch (const std::exception& error) {
+    std::fprintf(stderr, "%s: %s\n", program, error.what());
+    return EXIT_FAILURE;
+  }
 }
